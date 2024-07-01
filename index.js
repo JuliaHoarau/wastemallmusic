5fb3b5e1d7e352384740b99a35fabb3062712c1a
Julia Hoarau <j.hoarau97@outlook.com>
2024-07-02 05:12:10 +1000

    added animation to home page

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-document.querySelectorAll('.song-cover').forEach(function (cover) {
-    cover.addEventListener('click', function () {
-        // Toggle visibility of overlay on click
-        var overlay = cover.querySelector('.song-overlay');
-        overlay.style.opacity = overlay.style.opacity === '1' ? '0' : '1';
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function () {
+    // Logo animation
+    var logo = document.getElementById('header-logo');
+    var headerH1 = document.getElementById('latest-release-h1');
+
+    if (logo) {
+        logo.classList.add('animate');
+    }
+
+    // Add animation to h1 after the logo animation
+    setTimeout(function () {
+        if (headerH1) {
+            headerH1.classList.add('animate');
+        }
+    }, 1000); // Adjust this delay (in milliseconds) as needed
+
+    // Existing fade-in functionality
     var fadeIns = document.querySelectorAll('.section-h1, .section-h2');
 
     function checkPosition() {
         var windowHeight = window.innerHeight;
         fadeIns.forEach(function (fadeIn) {
             var position = fadeIn.getBoundingClientRect().top;
-            if (position < windowHeight - 50) {
+            if (position < windowHeight - 100) {
                 fadeIn.classList.add('visible');
             }
         });
